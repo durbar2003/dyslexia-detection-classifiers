b617f82846cbd9367c6823e767239ebed9a836a6
durbar chakrabarty <durbardibyo@gmail.com>
2022-09-18 11:52:03 +0530

    minor changes

--- SVM_classifier.py
+++ SVM_classifier.py
@@ -1,16 +1,9 @@
 #importing the libraries
 
-import numpy as np
 import pandas as pd
-from scipy.stats import mode
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
 
 df=pd.read_excel("./content/speech_data.xlsx")
 
